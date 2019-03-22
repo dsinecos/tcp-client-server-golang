158f032742f7c35d66152b717ee03533224f11ef
Divyanshu Singh <dsinecos@gmail.com>
2019-03-22 16:26:12 +0530

    add http demultiplexer on tcp server

--- tcp-server/main.go
+++ tcp-server/main.go
@@ -43,14 +43,42 @@ func handle(conn net.Conn) {
 			request[header[0]] = header[1]
 		}
 
-		if len(header) > 0 && header[0] == "Host:" {
-			body := fmt.Sprintf(`<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title></title></head><body><strong>%s</strong></body></html>`, fmt.Sprintf("URI : %s%s", header[1], request["GET"]))
-
-			fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
-			fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
-			fmt.Fprint(conn, "Content-Type: text/html\r\n")
-			fmt.Fprint(conn, "\r\n")
-			fmt.Fprint(conn, body)
+		if len(header) > 0 && header[0] == "GET" {
+			if header[1] == "/" {
+				homeRoute(conn)
+			} else {
+				catchAllRoute(conn)
+			}
 		}
 	}
+}
+
+func sendResponseHeaders(conn net.Conn, body string) {
+
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/html\r\n")
+	fmt.Fprint(conn, "\r\n")
+
+}
+
+func createResponseBody(s string) string {
+	body := fmt.Sprintf(`<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title></title></head><body><strong>%s</strong></body></html>`, s)
+	return body
+}
+
+func homeRoute(conn net.Conn) {
+
+	body := createResponseBody("You're on the Home Route")
+
+	sendResponseHeaders(conn, body)
+	fmt.Fprint(conn, body)
+}
+
+func catchAllRoute(conn net.Conn) {
+
+	body := createResponseBody("You're on the Catch All Route")
+
+	sendResponseHeaders(conn, body)
+	fmt.Fprint(conn, body)
 }
